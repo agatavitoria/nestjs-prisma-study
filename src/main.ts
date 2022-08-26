34f100e3178b1b7bcfb157ea26d7e4adf3a9e773
agata-vitoria <agatavitoriadelima@gmail.com>
2022-08-26 13:32:42 -0300

    comment removal

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { PrismaService } from './common/service';
 import { enableSwagger } from './swagger.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   enableSwagger(app);
   await app.listen(3000);
+  
+  const prismaService = app.get(PrismaService);
+  await prismaService.enableShutdownHooks(app);
 }
 bootstrap();
